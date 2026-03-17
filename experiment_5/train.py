ca697cb406d0c185b0d1fd704cb96ab842b3fd92
rohtanrt23-cell <roh.tan.rt23@dypatil.edu>
2026-03-17 10:31:24 +0530

    mlflow integration

--- experiment_5/train.py
+++ experiment_5/train.py
@@ -1,36 +1,73 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score, roc_curve, auc
 
 dataset = pd.read_csv("Social_Network_Ads.csv")
-dataset.head
-dataset.info
 
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
-print(X)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.30, random_state=0
+)
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30, random_state = 0)
-
-from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
-classifier.fit(X_train, y_train)
+mlflow.set_experiment("Logistic_Regression_Ads")
 
-y_pred = classifier.predict(X_test)
+C_value = 0.5
+max_iter = 200
 
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
+with mlflow.start_run():
 
-Accuracy=(74+31)/120
-Accuracy
+    classifier = LogisticRegression(C=C_value, max_iter=max_iter, random_state=0)
+    classifier.fit(X_train, y_train)
 
-Specificity=(74/74+5)
-Specificity
+    y_pred = classifier.predict(X_test)
+    y_prob = classifier.predict_proba(X_test)[:, 1]
+
+    cm = confusion_matrix(y_test, y_pred)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    fpr, tpr, thresholds = roc_curve(y_test, y_prob)
+    roc_auc = auc(fpr, tpr)
+
+    mlflow.log_param("C", C_value)
+    mlflow.log_param("max_iter", max_iter)
+
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_metric("roc_auc", roc_auc)
+
+    plt.figure()
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title("Confusion Matrix")
+    plt.colorbar()
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.savefig("confusion_matrix.png")
+    mlflow.log_artifact("confusion_matrix.png")
+
+    plt.figure()
+    plt.plot(fpr, tpr, label="ROC curve (area = %0.2f)" % roc_auc)
+    plt.plot([0, 1], [0, 1], linestyle='--')
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("ROC Curve")
+    plt.legend()
+    plt.savefig("roc_curve.png")
+    mlflow.log_artifact("roc_curve.png")
+
+    mlflow.sklearn.log_model(classifier, "logistic_model")
+
+    print("Confusion Matrix:\n", cm)
+    print("Accuracy:", accuracy)
+    print("ROC AUC:", roc_auc)
